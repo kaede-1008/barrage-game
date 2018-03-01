2c386b7dda6187a58f5b9f3db8aa1a28390722a7
kaede-1008 <kei.pomw@gmail.com>
2018-03-01 18:32:55 +0900

    player-change

--- baggage-game/player.h
+++ baggage-game/player.h
@@ -11,6 +11,12 @@ typedef struct {
 
 } Player;
 
+typedef struct {
+	int flag, kind, cnt, state, time;
+	double x, y, angle, spd, base_angle[1], rem_spd[1], r;
+	unsigned int color;
+} Shot;
+
 void player_move(void);
 void player_shot(void);
 void player_hit(Bullet *bullet);
